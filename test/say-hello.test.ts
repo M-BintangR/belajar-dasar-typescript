43703dac4270badef63ba576335eed0ea76cccd3
= <muhbintang650@gmail.com>
2023-12-25 09:59:08 +0800

    say hello function ✅

---
+++ test/say-hello.test.ts
@@ -0,0 +1,7 @@
+import { sayHello } from "../src/say-hello";
+
+describe('sayHello', function(){
+    it('should return hello bintang', function(){
+        expect(sayHello("Bintang")).toBe("Hello Bintang");
+    });
+});
