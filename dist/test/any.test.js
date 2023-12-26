fb443de3fa083d7ae7c15563658be28a6822c84c
= <muhbintang650@gmail.com>
2023-12-26 10:22:41 +0800

    tipe data any ✅

---
+++ dist/test/any.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe("Any", function () {
+    it('should support in javascript', function () {
+        const person = {
+            id: 1,
+            name: "Muhammad Bintang",
+            age: 19,
+        };
+        console.info(person);
+    });
+});
---
+++ test/any.test.ts
@@ -0,0 +1,13 @@
+describe("Any", function(){
+    it('should support in javascript', function(){
+        
+        const person : any = {
+            id: 1,
+            name: "Muhammad Bintang",
+            age: 19,
+        };
+
+        console.info(person);
+
+    });
+}); 
