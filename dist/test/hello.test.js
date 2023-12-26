d137958a2e1a17abbaa96f8427e28c92002d7b20
= <muhbintang650@gmail.com>
2023-12-26 09:44:24 +0800

    kompilasi typescript ✅

---
+++ dist/test/hello.test.js
@@ -0,0 +1,7 @@
+"use strict";
+describe('Hello', function () {
+    it("should sat hello", function () {
+        const name = "Hello Bintang";
+        expect(name).toBe("Hello Bintang");
+    });
+});
