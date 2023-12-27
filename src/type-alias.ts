e1053d36e76ce7814df82dc3a62dbc8ff1cd54c5
= <muhbintang650@gmail.com>
2023-12-27 21:46:38 +0800

    optional properties ✅

--- src/type-alias.ts
+++ src/type-alias.ts
@@ -3,6 +3,7 @@ export type ID = string | number; // type alias dengan type union
 export type Category = {
     id: ID,
     name: string,
+    description?: string,
 }
 
 export type Product = {
@@ -10,4 +11,5 @@ export type Product = {
     name: string,
     price: number,
     category: Category,
+    description?: string,
 }
